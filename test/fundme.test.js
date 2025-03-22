99bf026b231c54d62347f110670319fd8e4bcb32
chenbin <you@example.com>
2025-03-22 21:06:14 +0800

    TEST

--- test/fundme.test.js
+++ test/fundme.test.js
@@ -1,28 +1,28 @@
 const { assert } = require("chai");
 
 describe("test fundme contract", async function () {
-    // let fundMe
-    // const { firstAccount } = await getNamedAccounts()
-    // beforeEach(async function () {
-    //     await deployments.fixture(["All"])
-    //     // firstAccount = (await getNamedAccounts()).firstAccount
-    //     const fundMeDeployment = await deployments.get("FundMe")
-    //     fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address)
-    // })
+    let fundMe
+    let firstAccount
+    beforeEach(async function () {
+        await deployments.fixture(["All"])
+        firstAccount = (await getNamedAccounts()).firstAccount
+        const fundMeDeployment = await deployments.get("FundMe")
+        fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address)
+    })
 
     it("test if the owner is msg.sender", async function () {
-        const [firstAccount] = await ethers.getSigners()
-        console.log(firstAccount.address)
-        const fundMeFactory = await ethers.getContractFactory("FundMe")
-        const fundMe = await fundMeFactory.deploy(300)
+        // const [firstAccount] = await ethers.getSigners()
+        // console.log(firstAccount.address)
+        // const fundMeFactory = await ethers.getContractFactory("FundMe")
+        // const fundMe = await fundMeFactory.deploy(300)
         await fundMe.waitForDeployment()
-         assert.equal((await fundMe.owner()), firstAccount.address)
-        //assert.equal((await fundMe.owner()), firstAccount)
+        //assert.equal((await fundMe.owner()), firstAccount.address)
+        assert.equal((await fundMe.owner()), firstAccount)
     });
 
     it("test if the datafeed is assigned correctly", async function () {
-        const fundMeFactory = await ethers.getContractFactory("FundMe")
-        const fundMe = await fundMeFactory.deploy(300)
+        // const fundMeFactory = await ethers.getContractFactory("FundMe")
+        // const fundMe = await fundMeFactory.deploy(300)
         await fundMe.waitForDeployment()
         assert.equal((await fundMe.dataFeed()), 0x694AA1769357215DE4FAC081bf1f309aDC325306)
     })
