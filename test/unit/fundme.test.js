c31dc39961c54b9aaec1d518f684be780fe207d4
chenbin <you@example.com>
2025-03-28 00:00:22 +0800

    测试

--- test/unit/fundme.test.js
+++ test/unit/fundme.test.js
@@ -16,8 +16,10 @@ describe("test fundme contract", async function () {
         secondAccount = (await getNamedAccounts()).secondAccount
         const fundMeDeployment = await deployments.get("FundMe")
         mockV3Aggregator = await deployments.get("MockV3Aggregator")
+                 const [signer1, signer2] = await ethers.getSigners()
+        //         fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address,signer1)
+        // console.log(fundMe.runner.address);
         fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address)
-
         fundMeSecondAccount = await ethers.getContract("FundMe", secondAccount)
     })
 
